b89cef80f2967626565fb4b799cc059e0e04a6a2
Welington Fidelis <welingtonfidelis@gmail.com>
2021-06-28 21:11:13 -0300

    Using js lib to 3d rotate

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
 (perspectiveHover = () => {
-  const intensity = 12;
-  const cardElement = document.querySelector(".card");
+  // const intensity = 12;
+  // const cardElement = document.querySelector(".card");
 
-  cardElement.addEventListener("mousemove", (e) => {
-    const el = e.target;
-    const elX = el.offsetLeft;
-    const elY = el.offsetTop;
-    const elWidth = el.offsetWidth;
-    const elHeight = el.offsetHeight;
+  // cardElement.addEventListener("mousemove", (e) => {
+  //   const el = e.target;
+  //   const elX = el.offsetLeft;
+  //   const elY = el.offsetTop;
+  //   const elWidth = el.offsetWidth;
+  //   const elHeight = el.offsetHeight;
 
-    const mouseX = e.pageX;
-    const mouseY = e.pageY;
+  //   const mouseX = e.pageX;
+  //   const mouseY = e.pageY;
 
-    const rotateY = -(((elWidth / 2 - (mouseX - elX)) / (elWidth / 2)) * intensity);
-    const rotateX = ((elHeight / 2 - (mouseY - elY)) / (elHeight / 2)) * intensity;
+  //   const rotateY = -(((elWidth / 2 - (mouseX - elX)) / (elWidth / 2)) * intensity);
+  //   const rotateX = ((elHeight / 2 - (mouseY - elY)) / (elHeight / 2)) * intensity;
 
-    el.style.transition = "";
-    el.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-  });
+  //   el.style.transition = "";
+  //   el.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  // });
 
-  cardElement.addEventListener("mouseout", (e) => {
-    e.target.style.transition = ".5s";
-    e.target.style.transform = `rotateX(0deg) rotateY(0deg)`;
-  });
+  // cardElement.addEventListener("mouseout", (e) => {
+  //   e.target.style.transition = ".5s";
+  //   e.target.style.transform = `rotateX(0deg) rotateY(0deg)`;
+  // });
+
+  VanillaTilt.init(document.querySelector(".card"), {
+		max: 20,
+		speed: 400,
+    glare: true
+	});
+	
+	VanillaTilt.init(document.querySelectorAll(".card"));
 })();
